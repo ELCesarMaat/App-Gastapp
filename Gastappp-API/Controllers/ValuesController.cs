7a86d12aae23aa265fa992be8f5f0771cba5b519
ELCesarMaat <cesarmaat@hotmail.com>
2025-05-11 15:53:12 -0600

    Se agrego controlador de prueba

---
+++ Gastappp-API/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gastappp_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetTime(string name, int age)
+        {
+            return Ok($" Hola {name} de edad {age} son las {DateTime.Now.ToLongTimeString()}");
+        }
+    }
+}
