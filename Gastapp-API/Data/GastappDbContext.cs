8343c90d763fb8c8c7cad0a8bf43df03f1d2a4d5
ELCesarMaat <cesarmaat@hotmail.com>
2025-05-13 16:20:27 -0600

    Railway config

---
+++ Gastapp-API/Data/GastappDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Gastapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gastapp_API.Data
+{
+    public class GastappDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Spending> Spending { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public GastappDbContext(DbContextOptions<GastappDbContext> options) : base(options)
+        {
+
+        }
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
--- Gastapp.Models/Models/User.cs
+++ Gastapp.Models/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,9 @@ namespace Gastapp.Models
 {
     public class User
     {
-        public string LocalUserId { get; set; } = null!;
+        [Key]
         public string? OnlineUserId { get; set; }
+        public string LocalUserId { get; set; } = null!;
         public decimal Salary { get; set; }
         public string Name { get; set; } = null!;
         public string? Email { get; set; }
