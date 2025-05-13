fefb74a3e76dfb3dc19782a7eafee19fcc35d2aa
ELCesarMaat <cesarmaat@hotmail.com>
2025-05-13 17:51:37 -0600

    Se agrego el primer controller

---
+++ Gastapp-API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gastapp_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+        }
+
+        [HttpGet("/user/Hello")]
+        public IActionResult HelloApi()
+        {
+            return Ok("Hello from UserController");
+        }
+    }
+}
