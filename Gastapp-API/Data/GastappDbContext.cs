2f24879651be5b29b8a52220774060d29756c464
ELCesarMaat <cesarmaat@hotmail.com>
2025-05-13 18:54:12 -0600

    cambios en bdd

--- Gastapp-API/Data/GastappDbContext.cs
+++ Gastapp-API/Data/GastappDbContext.cs
@@ -10,9 +10,19 @@ namespace Gastapp_API.Data
         public DbSet<IncomeType> IncomeTypes { get; set; } = null!;
         public DbSet<Category> Categories { get; set; } = null!;
         public DbSet<Spending> Spendings { get; set; } = null!;
+
         public GastappDbContext(DbContextOptions<GastappDbContext> options) : base(options)
         {
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IncomeType>().HasData(
+                new IncomeType { IncomeTypeId = 1, IncomeTypeName = "Semanal" },
+                new IncomeType { IncomeTypeId = 2, IncomeTypeName = "Quincenal" },
+                new IncomeType { IncomeTypeId = 3, IncomeTypeName = "Mensual" }
+            );
         }
     }
 }
---
+++ Gastapp-API/Migrations/20250514004754_initial2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Gastapp_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "IncomeTypes",
+                columns: new[] { "IncomeTypeId", "IncomeTypeName" },
+                values: new object[,]
+                {
+                    { 1, "Semanal" },
+                    { 2, "Quincenal" },
+                    { 3, "Mensual" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "IncomeTypes",
+                keyColumn: "IncomeTypeId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "IncomeTypes",
+                keyColumn: "IncomeTypeId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "IncomeTypes",
+                keyColumn: "IncomeTypeId",
+                keyValue: 3);
+        }
+    }
+}
