a69d824a1ddf496081dc1949a564633918900e81
Néstor Daniel <daniel00moore@outlook.es>
2025-05-18 00:13:07 -0600

    Create: Se crea carpeta de validaciones y se crea la validacion de registro en el usuario.

---
+++ Gastapp/Validators/RegisterValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Gastapp.ViewModels;
+
+
+namespace Gastapp.Validators
+{
+    internal class RegisterValidator : AbstractValidator<RegisterViewModel>
+    {
+        public RegisterValidator()
+        {
+
+        }
+    }
+}
